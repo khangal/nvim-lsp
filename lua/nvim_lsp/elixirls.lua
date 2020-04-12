cfba3fb93c36b84ebc6ed9ddcc3e40b9e20bf6b8
Khangal Jargalsaikhan <khangal.j@irbis.sg>
2020-04-12 16:04:05 +0800

    add elixirls support

---
+++ lua/nvim_lsp/elixirls.lua
@@ -0,0 +1,22 @@
+local configs = require 'nvim_lsp/configs'
+local util = require 'nvim_lsp/util'
+
+configs[server_name] = {
+  default_config = {
+    cmd = {"elixirls"};
+    filetypes = {"elixir", "eex"}
+    root_dir = util.root_pattern("mix.exs", ".git");
+  };
+
+  docs = {
+    description = [[
+https://github.com/elixir-lsp/elixir-ls
+
+Elixir language server.
+]];
+    default_config = {
+      root_dir = [[root_pattern("mix.exs", ".git")]];
+    };
+  };
+}
+-- vim:et ts=2 sw=2
